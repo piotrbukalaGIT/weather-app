b43f26b1c15f104dbcac42f854363a2b86146184
piotrbukalaGIT <pbukala@gmail.com>
2023-03-11 21:34:54 +1100

    weather-app express

---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const locationMsg = document.querySelector('#location')
+const forecastMsg = document.querySelector('#forecast')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                locationMsg.textContent = data.location
+                forecastMsg.textContent = data.forecast
+                // console.log(data.location)
+                // console.log(data.forecast)
+            }
+        })
+    })
+})
