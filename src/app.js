1d1babfd27051607406bd7d22d9a1108ceb24a91
piotrbukalaGIT <pbukala@gmail.com>
2023-03-11 22:08:42 +1100

    Styling fixes

--- src/app.js
+++ src/app.js
@@ -38,22 +38,22 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Andrew Mead'
+        name: 'Piotr'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
-        title: 'About Me',
-        name: 'Andrew Mead'
+        title: 'O mnie',
+        name: 'Piotr'
     })
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: 'This is some helpful text.',
+        helpText: 'Tutaj bedzie pomoc',
         title: 'Help',
-        name: 'Andrew Mead'
+        name: 'Piotr'
     })
 })
 
@@ -90,7 +90,7 @@ app.get('/weather', (req, res) => {
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Piotr',
         errorMessage: 'Help article not found.'
     })
 })
@@ -98,7 +98,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Piotr',
         errorMessage: 'Page not found.'
     })
 })
