cc4bc0a3748cef112be1f1f91f89d426aa10c144
piotrbukalaGIT <pbukala@gmail.com>
2023-03-11 22:00:05 +1100

    fix app.js

--- src/app.js
+++ src/app.js
@@ -25,11 +25,11 @@ app.use(express.static(publicDirectoryPath))
 
 
 // AWS EB
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
